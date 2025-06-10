3129a00ed778657e5c41b77885e3e54beb8e2518
Stephanie <200150677+Steph7478@users.noreply.github.com>
2025-06-10 08:36:36 -0300

    feat: navbar

---
+++ src/common/Navbar.tsx
@@ -0,0 +1,59 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav
+      style={{
+        background: "var(--navbar)",
+        width: "100%",
+        height: "80px",
+      }}
+      className="flex-center"
+    >
+      <ul
+        style={{
+          width: "90%",
+          padding: "0px",
+          flexDirection: "row",
+          flexWrap: "wrap",
+          gap: "10px",
+        }}
+        className="flex-between"
+      >
+        <Link
+          href={"/"}
+          style={{
+            height: "100%",
+          }}
+          className="flex-center"
+        >
+          <Image
+            src="https://lacreisaude.com.br/_next/static/media/logo_lacrei_desktop.7ae004ab.svg"
+            width={187}
+            height={24}
+            priority
+            className="logo"
+            alt="Home"
+          />
+        </Link>
+
+        <li
+          className="flex-center"
+          style={{
+            listStyle: "none",
+            width: "150px",
+            height: "30px",
+          }}
+        >
+          <Link className="link-hover flex-center" href="/">
+            Quem somos
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
